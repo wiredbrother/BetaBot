14525bea21cd4648aef19bf27d4a5792e8b7ad7d
wiredbrother <34851412+wiredbrother@users.noreply.github.com>
2017-12-30 23:56:04 -0700

    That's All Folks: The sequel!

--- commands.lua
+++ commands.lua
@@ -71,10 +71,10 @@ end
 local rules = {
 "&6*&rUse Common Sense",
 "&6*&rNo Griefing Claimed Land", 
-"&6*&rYou are Not a Super Hacker, Don't Hack", 
+"&6*&rYou are Not a Super Hacker, Dont Hack", 
 "&6*&rRespect All Players",
 "&6*&rNo Trolling/Flaming",
-"&6*&rJust Don't Be Dumb",
+"&6*&rJust Dont Be Dumb",
 "&6*&rRespect Admins (people with pink (Or green) names)",
 "&6*&rOh and Have Fun!"
 }
